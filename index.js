841bd0896527eb3d89a2a442f90d16f5823e4647
dianafabela8 <fabeladiana.df@gmail.com>
2023-08-30 19:33:29 -0700

    created functions for license link and section

--- index.js
+++ index.js
@@ -1,13 +1,82 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    message: "What is your Title?",
+    name: "title",
+  },
+  {
+    type: "input",
+    message: "What is your Description?",
+    name: "description",
+  },
+  {
+    type: "input",
+    message: "What is your Installation instructions?",
+    name: "installation",
+  },
+  {
+    type: "input",
+    message: "What is your Usage information?",
+    name: "usage",
+  },
+  {
+    type: "list",
+    message: "What is your License?",
+    name: "license",
+    choices: ["test", 'test2', 'test3', 'None']
+  },
+  {
+    type: "input",
+    message: "What is your Usage?",
+    name: "usage",
+  },
+  {
+    type: "input",
+    message: "What is your Contribution guidelines?",
+    name: "contribution",
+  },
+  {
+    type: "input",
+    message: "What is your test instructions?",
+    name: "test",
+  },
+  {
+    type: "input",
+    message: "What are your questions?",
+    name: "question",
+  },
+  {
+    type: "input",
+    message: "What are your GitHub username?",
+    name: "username",
+  },
+  {
+    type: "input",
+    message: "What are your email?",
+    name: "email",
+  },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (error, data) =>
+    error ? console.error(error) : console.log(data)
+  );
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions).then((answers) => {
+    const markdown = generateMarkdown(answers);
+    writeToFile("testreadme.md", markdown);
+  });
+}
 
 // Function call to initialize app
 init();
---
+++ testreadme.md
@@ -0,0 +1,43 @@
+# 1
+
+  ## Description
+  
+  1
+  
+  ## Table of Contents 
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contribution Guidelines](#contribution_guidelines)
+  - [License](#license)
+  - [Tests](#tests)
+  
+  ## Installation
+  
+  1
+  
+  ## Usage
+  
+  1
+  
+  ## Contribution Guidelines
+  
+  1
+  
+  
+  ## License 
+  test2
+  
+  ## Badges
+  
+  
+  
+  ## Features
+    
+  
+  
+  ## Tests
+  
+  1
+  
+  
